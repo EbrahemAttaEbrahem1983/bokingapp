96ea62864e816a59935f4790178a7df48ffb8e89
Ebrahem Atta <ebrahem.atta.ebrahem@gmail.com>
2025-10-18 09:12:34 +0300

    chore: scaffold feature-first folders for bookings feature

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../features/bookings/presentation/pages/bookings_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BokingApp',
+      theme: ThemeData(useMaterial3: true),
+      home: const BookingsPage(), // أو UnitsPage لو لسه
+    );
+  }
+}
